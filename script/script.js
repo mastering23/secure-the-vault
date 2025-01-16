c928c0f4ebae5fb2cb8ae8e9afa19302d3b4b8e4
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-16 13:13:46 -0500

    adding final changes

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 //declare val
+
 let num1 = 0;
 let num2 = 0;
 let num3 = 0;
@@ -10,12 +11,16 @@ num1 = 5 + 5;
 num2 = 20 * 2;
 num3 = 40 - 1;
 
+//adding final const variables  + message
 
 
 const finalnum1 = num1;
 const finalnum2 = num2;
 const finalnum3 = num3;
 
-console.log(finalnum1);
-console.log(finalnum2);
-console.log(finalnum3);
+const message = 
+`You are winner 😁 and here is 
+your numbers for the secure vault "💸${finalnum1} ${finalnum2} ${finalnum3}!💸"`;
+
+//adding the alert message
+alert(message);
