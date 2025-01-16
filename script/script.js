dd23ba3225d8e453907c889b0ce6a8db2ceb90d0
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-16 13:32:45 -0500

    adding message

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
-//declare val
+
+
+//declare variables
 
 let num1 = 0;
 let num2 = 0;
@@ -11,16 +13,20 @@ num1 = 5 + 5;
 num2 = 20 * 2;
 num3 = 40 - 1;
 
-//adding final const variables  + message
+//adding final const variables  
 
 
 const finalnum1 = num1;
 const finalnum2 = num2;
 const finalnum3 = num3;
 
-const message = 
-`You are winner 😁 and here is 
-your numbers for the secure vault "💸${finalnum1} ${finalnum2} ${finalnum3}!💸"`;
+//create a message
 
-//adding the alert message
+const message = 
+
+`🎉You are a winner 😁🎉 and here is 
+your numbers for the secure vault "💸🔒${finalnum1} ${finalnum2} ${finalnum3}!🔒💸"`;
+
+//adding the alert function (message)
+
 alert(message);
