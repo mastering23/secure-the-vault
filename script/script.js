417c57e5a9179e8c41886c50312cd95fcd757b27
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-16 13:01:56 -0500

    adding val and cal

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
-console.log("Hi there !")
+//declare val
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+
+
+// calculation  10 - 40 - 39
+
+num1 = 5 + 5;
+num2 = 20 * 2;
+num3 = 40 - 1;
+
+
+
+const finalnum1 = num1;
+const finalnum2 = num2;
+const finalnum3 = num3;
+
+console.log(finalnum1);
+console.log(finalnum2);
+console.log(finalnum3);
